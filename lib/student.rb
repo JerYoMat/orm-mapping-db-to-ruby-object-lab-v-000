8c82e3a28bfb09fa0e0618bf37e905e7f9e26fb1
Jeremy Emata <jeremy.emata@gmail.com>
2018-11-06 04:26:18 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -30,6 +30,16 @@ class Student
       self.new_from_db(row)
     end
   end 
+  
+  def self.students_below_12_grade 
+     sql = <<-SQL
+      SELECT *
+      FROM students WHERE grade < 12
+    SQL
+    DB[:conn].execute(sql).map do |row|
+      self.new_from_db(row)
+    end
+  end 
 
   def self.find_by_name(name)
     # find the student in the database given a name

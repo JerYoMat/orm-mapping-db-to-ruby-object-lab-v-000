55c0cc552720870cc611593e97080923e62aafe1
Jeremy Emata <jeremy.emata@gmail.com>
2018-11-06 04:16:30 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -3,7 +3,9 @@ class Student
 
   def self.new_from_db(row)
     # create a new Student object given a row from the database
-    
+    sql = <<-SQL
+      SELECT name FROM students where id=?
+    SQL
   end
 
   def self.all
